d7f312bf6a397f63f2f44a7d86c2754f14709579
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2022-06-09 22:03:36 +0200

    update profile bug fix

--- actions/editProfile/editProfile.inc.php
+++ actions/editProfile/editProfile.inc.php
@@ -7,11 +7,10 @@ $value = $_POST['value'];
 
 $profile_text =  DB::run("SELECT PR_content FROM profiles WHERE PR_acc_id = ?", [$session_id])->fetchColumn();
 
-
-if (!$profile_text) {
-    DB::run("INSERT INTO profiles (PR_content, PR_acc_id) VALUES (?,?)", [$value, $session_id]);
-} else {
+if ($profile_text) {
     DB::run("UPDATE profiles SET PR_content = ? WHERE PR_acc_id = ?", [$value, $session_id]);
+} else {
+    DB::run("INSERT INTO profiles (PR_content, PR_acc_id) VALUES (?,?)", [$value, $session_id]);
 }
 
 echo 'Profilen ble oppdatert!' . '<|>' . 'success';
