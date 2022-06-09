9fef9ce7e6213ac252aa70af6bbc5342b2edd90d
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2022-06-09 20:56:45 +0200

    error for number below 0

--- actions/bank/money_in.inc.php
+++ actions/bank/money_in.inc.php
@@ -8,8 +8,8 @@ $value = removeSpace($_POST['value']);
 $money_bank = DB::run("SELECT AS_bankmoney FROM account_stat WHERE AS_id=?", [$session_id])->fetchColumn();
 $money_hand = DB::run("SELECT AS_money FROM account_stat WHERE AS_id=?", [$session_id])->fetchColumn();
 
-if (!is_numeric($value)) {
-    echo 'Ugyldig input. Kun tall er tillatt. ' . '<|>' . 'danger';
+if (!is_numeric($value) || $value <= 0) {
+    echo 'Ugyldig input. Kun hele tall over 0 er tillatt. ' . '<|>' . 'danger';
 } elseif ($value && is_numeric($value) && $value <= $money_hand) {
     $stmt = DB::run("UPDATE account_stat SET AS_money = AS_money - ?, AS_bankmoney = AS_bankmoney + ? WHERE AS_id = ?", [$value, $value, $session_id]);
     $stmt->rowCount();
