67871b5250275af896e140a1cdebf8cd1ae1dca1
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2022-06-09 22:16:36 +0200

    display profile text and mock up data

--- actions/myProfile/myProfile.php
+++ actions/myProfile/myProfile.php
@@ -11,12 +11,14 @@ if (!isset($_GET['id'])) {
 
 $ACC_row =  DB::run("SELECT * FROM account WHERE ACC_id = ?", [$id])->fetch();
 $AS_row =   DB::run("SELECT * FROM account_stat WHERE AS_id = ?", [$id])->fetch();
+$PR_row =   DB::run("SELECT PR_content FROM profiles WHERE PR_acc_id = ?", [$id])->fetch();
 
 $usename =      $ACC_row['ACC_username'];
 $created =      $ACC_row['ACC_register'];
 $last_active =  $ACC_row['ACC_last_active'];
 $exp =          $AS_row['AS_EXP'];
 $money =        $AS_row['AS_money'] + $AS_row['AS_bankmoney'];
+$profile_text = $PR_row['PR_content'];
 
 ?>
 
@@ -29,7 +31,41 @@ $money =        $AS_row['AS_money'] + $AS_row['AS_bankmoney'];
         </div>
         <div class="card-body">
             <div class="row align-items-center">
-                <!-- <img class="center-image" style="width: auto;" src="actions/#/img/#" /> -->
+                <div class="col-7 center-image-flex">
+                    <img style="max-width: 300px; max-height: 300px;" src="https://i.imgur.com/DjyB4bo.png" />
+                </div>
+                <div class="col-5">
+                    <div class="row">
+                        <div class="col-6">
+                            <p class="h3">Brukernavn</p>
+                            <address>
+                                Rolle<br>
+                                Rank<br>
+                                Drap<br>
+                                Sist aktiv<br>
+                                Registrert
+                            </address>
+                        </div>
+                        <div class="col-6 text-end">
+                            <p class="h3">Skitzo</p>
+                            <address>
+                                Administrator<br>
+                                Mafioso<br>
+                                100<br>
+                                I dag 15:00<br>
+                                I går
+                            </address>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card-body" style="border-top: none;">
+            <div class="row align-items-center">
+                <div class="hr-text">
+                    <span>Profil</span>
+                </div>
+                <?= $profile_text ?>
             </div>
         </div>
     </div>
