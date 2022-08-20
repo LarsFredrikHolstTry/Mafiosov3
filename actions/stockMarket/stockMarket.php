b87b5571d30d43133842f27c3c7a4f51f58b9711
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2022-08-20 17:59:27 +0200

    small fix and beta link

--- actions/stockMarket/stockMarket.php
+++ actions/stockMarket/stockMarket.php
@@ -10,6 +10,47 @@ include '../../global-variables.php';
     <div class="card">
         <div class="card-header">
             <h3 class="card-title">
+                <h3 class="card-title text-capitalize">Market</h3>
+            </h3>
+            <div class="ms-auto">
+                <span hx-get="actions/faq/faq.php" hx-trigger="click" hx-target="#container" hx-swap="outerHTML" class="form-help">?</span>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="empty">
+                <div class="empty-img"><img src="img/noResult.svg" height="128" alt="">
+                </div>
+                <p class="empty-title">Funksjonen er under utvikling</p>
+                <p class="empty-subtitle text-muted">
+                    Denne funksjonen er for tiden under utvikling og vil lanseres fortløpende
+                    <br><a class="mt-2 btn btn-facebook" target="_blank" href="https://discord.gg/CsZht4W4Cb">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-discord" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <circle cx="9" cy="12" r="1"></circle>
+                            <circle cx="15" cy="12" r="1"></circle>
+                            <path d="M7.5 7.5c3.5 -1 5.5 -1 9 0"></path>
+                            <path d="M7 16.5c3.5 1 6.5 1 10 0"></path>
+                            <path d="M15.5 17c0 1 1.5 3 2 3c1.5 0 2.833 -1.667 3.5 -3c.667 -1.667 .5 -5.833 -1.5 -11.5c-1.457 -1.015 -3 -1.34 -4.5 -1.5l-1 2.5"></path>
+                            <path d="M8.5 17c0 1 -1.356 3 -1.832 3c-1.429 0 -2.698 -1.667 -3.333 -3c-.635 -1.667 -.476 -5.833 1.428 -11.5c1.388 -1.015 2.782 -1.34 4.237 -1.5l1 2.5"></path>
+                        </svg>
+                        Bli med i vår discord for oppdateringer!</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- <?php
+
+        include '../../global-variables.php';
+
+        ?>
+<div class="col-12" id="container">
+
+    <div id="feedback-container"></div>
+
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">
                 <h3 class="card-title text-capitalize">Aksjemarked</h3>
             </h3>
             <div class="ms-auto">
@@ -168,4 +209,4 @@ include '../../global-variables.php';
             $("#feedback-container").load("components/feedback.php");
         })
     })
-</script>
+</script> -->
---
+++ betaperiode.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Mafioso v3 - Betaperiode</title>
+
+    <!-- HTMX -->
+    <script src="https://unpkg.com/htmx.org@1.3.3"></script>
+
+    <!-- Ajax -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <!-- Flat Icon interface icons -->
+    <link rel="stylesheet" href="https://cdn-uicons.flaticon.com/uicons-regular-straight/css/uicons-regular-straight.css" />
+
+    <!-- Tabler css and JS -->
+    <script src="https://unpkg.com/@tabler/core@latest/dist/js/tabler.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/@tabler/core@latest/dist/css/tabler.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@tabler/icons@latest/iconfont/tabler-icons.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@tabler/core@1.0.0-beta10/dist/css/tabler-flags.min.css" />
+
+    <!-- Local styling -->
+    <link rel="stylesheet" href="styling/styling.css" />
+    <link rel="icon" type="image/x-icon" href="img/logo/favicon.ico" />
+    <noscript>Javascript is required to run
+        Mafioso.</noscript>
+</head>
+
+<body class="border-primary d-flex flex-column theme-dark" cz-shortcut-listen="true">
+    <div class="page-body">
+        <div class="container-xl">
+            <div class="card card-lg">
+                <div class="card-body">
+                    <div class="space-y-4">
+                        <div>
+                            <h2 class="mb-3">
+                                Beta
+                            </h2>
+                            <div id="faq-1" class="accordion" role="tablist" aria-multiselectable="true">
+                                <div class="accordion-item">
+                                    <div class="accordion-header" role="tab">
+                                        <button class="accordion-button" data-bs-toggle="collapse" data-bs-target="#faq-1-0" aria-expanded="false">
+                                            Betaperiode
+                                        </button>
+                                    </div>
+                                    <div id="faq-1-0" class="accordion-collapse" role="tabpanel" data-bs-parent="#faq-1">
+                                        <div class="accordion-body pt-0">
+                                            <div>
+                                                <p>
+                                                    Mafioso åpnet 20. August 18:00 en betaperiode. Dette betyr at det kan være funksjoner som ikke er ferdigstilte.<br>
+                                                    Funksjoner kan også ha feil i seg og disse skal rapporteres til Skitzo på Discord!<br>
+                                                    Det vil også bli gitt informasjon på discord om hva som skjer så gjerne bli med på discorden vår.
+                                                </p>
+                                                <a class="mt-2 btn btn-facebook" target="_blank" href="https://discord.gg/CsZht4W4Cb">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-discord" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                        <circle cx="9" cy="12" r="1"></circle>
+                                                        <circle cx="15" cy="12" r="1"></circle>
+                                                        <path d="M7.5 7.5c3.5 -1 5.5 -1 9 0"></path>
+                                                        <path d="M7 16.5c3.5 1 6.5 1 10 0"></path>
+                                                        <path d="M15.5 17c0 1 1.5 3 2 3c1.5 0 2.833 -1.667 3.5 -3c.667 -1.667 .5 -5.833 -1.5 -11.5c-1.457 -1.015 -3 -1.34 -4.5 -1.5l-1 2.5"></path>
+                                                        <path d="M8.5 17c0 1 -1.356 3 -1.832 3c-1.429 0 -2.698 -1.667 -3.333 -3c-.635 -1.667 -.476 -5.833 1.428 -11.5c1.388 -1.015 2.782 -1.34 4.237 -1.5l1 2.5"></path>
+                                                    </svg>
+                                                    Bli med i vår discord for oppdateringer!</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
